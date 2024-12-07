4edebd2d27a99dc60c4216a4067df532875d7ffa
Arshia-r0 <arshia009k@proton.me>
2024-12-07 10:53:10 +0330

    log ui fix for user show

--- Modules/Order/routes/admin.php
+++ Modules/Order/routes/admin.php
@@ -5,8 +5,8 @@ use Modules\Order\Http\Controllers\admin\OrderController;
 Route::controller(OrderController::class)->prefix('/order')->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('{order}', 'show')->name('show');
-    Route::patch('/{order}', 'update')->name('update');
-    Route::delete('/{order}', 'destroy')->name('destroy');
-    Route::get('/create', 'create')->name('create');
-    Route::post('/create', 'store')->name('store');
+    Route::patch('{order}', 'update')->name('update');
+    Route::delete('{order}', 'destroy')->name('destroy');
+    Route::get('create', 'create')->name('create');
+    Route::post('create', 'store')->name('store');
 });
