60d13824477874ec37cfa1fa6d1dc4d450b82252
Arshia-r0 <arshia009k@proton.me>
2024-12-07 20:06:58 +0330

    create joboffer

---
+++ Modules/JobOffer/database/seeders/JobOfferDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\JobOffer\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class JobOfferDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
