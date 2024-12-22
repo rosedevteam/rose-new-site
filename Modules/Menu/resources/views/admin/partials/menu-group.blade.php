2cfdd126814af65d8ba832e857af9ffb13e83f1d
FarshadRajabzade <farshad.rajabzade@yahoo.com>
2024-12-22 09:49:20 +0330

    menu delete modal added

--- Modules/Menu/resources/views/admin/partials/menu-group.blade.php
+++ Modules/Menu/resources/views/admin/partials/menu-group.blade.php
@@ -12,11 +12,9 @@
         </td>
         <td>{{ verta($menu->created_at)->formatJalaliDateTime() }}</td>
         <td>
-            <div class="d-inline-block text-nowrap">
-                <a href="" class="btn btn-sm btn-danger">
-                    حذف
-                </a>
-            </div>
+            @can('delete-posts')
+                <x-admin::deletebutton/>
+            @endcan
         </td>
 
         @if($menu->children->count())
@@ -24,6 +22,29 @@
             ['menus' => $menu->children , 'child' => true , 'level' => $level+1])
         @endif
     </tr>
+    <div class="modal fade" id="delete-modal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="text-center mb-4 mt-0 mt-md-n2">
+                        <h3 class="secondary-font">آیا اطمینان دارید؟</h3>
+                    </div>
+                    <form id="deleteForm" action="{{ route("admin.menus.destroy", $menu) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <div class="col-12 text-center mt-4">
+                            <button type="submit" class="btn btn-danger me-sm-3 me-1">حذف</button>
+                            <button type="reset" class="btn btn-label-secondary" data-bs-dismiss="modal"
+                                    aria-label="Close">
+                                انصراف
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endforeach
 
 
