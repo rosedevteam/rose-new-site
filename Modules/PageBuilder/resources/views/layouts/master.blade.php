8cff4a87d7ee6fb8413e9ab9a004c8724220976f
FarshadRajabzade <farshad.rajabzade@yahoo.com>
2024-12-26 13:13:46 +0330

    add csrf token to master.blade.php

--- Modules/PageBuilder/resources/views/layouts/master.blade.php
+++ Modules/PageBuilder/resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- Favicon -->
@@ -46,7 +47,7 @@
 
 <!--script-->
 
-{{--<script src="{{asset('assets/front/js/jquery.js')}}"></script>--}}
+<script src="{{asset('assets/front/js/jquery.js')}}"></script>
 
 {{--<script src="{{asset('assets/front/js/jquery-ui.min.js')}}"></script>--}}
 
