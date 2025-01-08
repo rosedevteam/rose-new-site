ffc465c68acf72a264a77130b7bafcc964cd5e3a
Farshad <farshad.rajabzade@yahoo.com>
2025-01-08 11:09:04 +0330

    Front: login - register

---
+++ Modules/Auth/app/Http/Controllers/front/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Auth\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function auth()
+    {
+        
+    }
+}
