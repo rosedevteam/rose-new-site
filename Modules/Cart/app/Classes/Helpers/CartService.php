ca2bbdd37618671bc9946ae49a34b5afe7370ada
Arshia-r0 <arshia009k@proton.me>
2025-01-13 16:30:02 +0330

    validate discount

--- Modules/Cart/app/Classes/Helpers/CartService.php
+++ Modules/Cart/app/Classes/Helpers/CartService.php
@@ -5,8 +5,8 @@ namespace Modules\Cart\Classes\Helpers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Str;
-use Modules\Discount\Database\Seeders\DiscountDatabaseSeeder;
 use Modules\Discount\Models\Discount;
+use Modules\Product\Models\Product;
 
 class CartService
 {
@@ -193,13 +193,25 @@ class CartService
     protected function checkDiscountValidate($item, $discount)
     {
         $discount = Discount::where('code', $discount)->where('is_active', 1)->first();
-        if ($discount && $discount->expires_at > now()) {
-            if (
-                (!$discount->products->count()) ||
-                in_array($item['product']->id, $discount->products->pluck('id')->toArray())) {
-                $item['discount_amount'] = $discount->amount;
-            }
+        $product = Product::findOrFail($item['product']->id);
+
+        if (is_Null($discount)) return $item;
+        if ($discount->expires_at < now()) return $item;
+        if (!$discount->products->contains($product)) return $item;
+        if ($discount->discountRecords()->where('user_id', auth()->id())->count() == $discount->limit) return $item;
+
+        if ($discount->type == 'amount') {
+            $item['discount_amount'] = $discount->amount;
+        } else {
+            $item['discount_amount'] = ($discount->amount / 100) * ($product->sale_price ?: $product->price);
         }
+
+//        if ($discount && $discount->expires_at > now()) {
+//            if (
+//                (!$discount->products->count()) ||
+//                in_array($item['product']->id, $discount->products->pluck('id')->toArray())) {
+//            }
+//        }
         return $item;
     }
 }
