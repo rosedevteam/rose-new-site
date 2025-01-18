296e9fb47297fd56c6357c0047e13973eab4c488
Arshia-r0 <arshia009k@proton.me>
2025-01-18 14:28:05 +0330

    logic for if should call api

--- Modules/Analytics/app/Http/Controllers/front/AnalyticsController.php
+++ Modules/Analytics/app/Http/Controllers/front/AnalyticsController.php
@@ -13,11 +13,6 @@ use Modules\Analytics\Models\Trade;
 
 class AnalyticsController extends Controller
 {
-    static function isMarketOpen()
-    {
-        return true;
-    }
-
     public function index()
     {
         return view('analytics::front.index');
@@ -30,83 +25,80 @@ class AnalyticsController extends Controller
 
     public function indices()
     {
-//        $indices = Index::query();
-//        if ($indices->first()?->created_at < now()->subDays(10)) {
-//            $indices->delete();
-//            $newIndices = ApiClient::indices();
-//            foreach ($newIndices as $index) {
-//                Index::create($index);
-//            }
-//        }
         return Index::all()->toJson(JSON_UNESCAPED_UNICODE);
     }
 
     public function company($coId)
     {
-        $company = Company::where('coID', $coId)->get();
-        return $company->toJson(JSON_UNESCAPED_UNICODE);
+        return Company::where('coID', $coId)->get()->toJson(JSON_UNESCAPED_UNICODE);
     }
 
     public function getIndex($indexId)
     {
-        $index = Index::where('indexID', $indexId)->get();
-        return $index->toJson(JSON_UNESCAPED_UNICODE);
+        return Index::where('indexID', $indexId)->get()->toJson(JSON_UNESCAPED_UNICODE);
     }
 
     public function trades($coId)
     {
         $trades = Trade::where('coID', $coId);
-        if (self::isMarketOpen() && $trades->first()?->created_at < now()->subMinutes(2)) {
+        if (self::shouldApiCall($trades)) {
             $trades->delete();
             $newTrade = ApiClient::trades($coId);
             foreach ($newTrade as $trade) {
                 Trade::create($trade);
             }
-            $trades = Trade::where('coID', $coId)->get();
         }
-        return $trades->toJson(JSON_UNESCAPED_UNICODE);
+        return Trade::where('coID', $coId)->get()->toJson(JSON_UNESCAPED_UNICODE);
     }
 
     public function legalTrades($coId)
     {
         $legalTrades = LegalTrade::where('com_ID', $coId);
-        if (self::isMarketOpen() && $legalTrades->first()?->created_at < now()->subMinutes(2)) {
+        if (self::shouldApiCall($legalTrades)) {
             $legalTrades->delete();
             $newTrade = ApiClient::trades($coId);
             foreach ($newTrade as $trade) {
                 LegalTrade::create($trade);
             }
-            $legalTrades = LegalTrade::where('coID', $coId)->get();
         }
-        return $legalTrades->toJson(JSON_UNESCAPED_UNICODE);
+        return LegalTrade::where('com_ID', $coId)->get()->toJson(JSON_UNESCAPED_UNICODE);
     }
 
     public function indexValues($indexId)
     {
-        $indexValues = IndexValue::where('indexID', $indexId)->get();
-        if (self::isMarketOpen() && $indexValues->first()?->created_at < now()->subMinutes(2)) {
+        $indexValues = IndexValue::where('indexID', $indexId);
+        if (self::shouldApiCall($indexValues)) {
             $indexValues->delete();
             $newIndex = ApiClient::trades($indexId);
             foreach ($newIndex as $indexValue) {
                 IndexValue::create($indexValue);
             }
-            $indexValues = IndexValue::where('coID', $indexId)->get();
         }
-        return $indexValues->toJson(JSON_UNESCAPED_UNICODE);
+        return IndexValue::where('indexID', $indexId)->get()->toJson(JSON_UNESCAPED_UNICODE);
     }
 
     public function bidAsk($coId)
     {
-        $bidAsks = BidAsk::where('coID', $coId)->get();
-        if (self::isMarketOpen() && $bidAsks->first()?->created_at < now()->subMinutes(2)) {
+        $bidAsks = BidAsk::where('coID', $coId);
+        if (self::shouldApiCall($bidAsks)) {
             $bidAsks->delete();
             $newBidAsks = ApiClient::trades($coId);
             foreach ($newBidAsks as $bidAsk) {
                 BidAsk::create($bidAsk);
             }
-            $bidAsks = BidAsk::where('coID', $coId)->get();
         }
-        return $bidAsks->toJson(JSON_UNESCAPED_UNICODE);
+        return BidAsk::where('coID', $coId)->get()->toJson(JSON_UNESCAPED_UNICODE);
+    }
+
+    private static function shouldApiCall($builderQuery)
+    {
+        return $builderQuery->count() == 0 || (self::isMarketOpen() && $builderQuery->first()?->created_at < now()->subMinutes(2));
+    }
+
+    static function isMarketOpen()
+    {
+        // todo
+        return true;
     }
 
 }
