87b01868ecd903e0bae294f6b8bbfe57b0f76c40
Arshia-r0 <arshia009k@proton.me>
2025-01-18 10:52:40 +0330

    make apiClient static and add getToken method

--- Modules/Analytics/app/Http/client/ApiClient.php
+++ Modules/Analytics/app/Http/client/ApiClient.php
@@ -8,59 +8,62 @@ use Illuminate\Http\Client\ConnectionException;
 
 class ApiClient
 {
-    private string $url = 'https://data3.nadpco.com/api/';
+    private const url = "https://data3.nadpco.com/api/";
 
     private static function getToken()
     {
-        return Cache::get('nadpco-api-token');
+        $token = Cache::get('nadpco-api-token');
+        if (!$token) {
+            $token = self::token();
+        }
+        return $token;
     }
 
-    public function token()
+    private static function token()
     {
-        Cache::remember('nadpco-api-token', 60 * 24, function () {
+        return Cache::remember('nadpco-api-token', 60 * 24, function () {
             try {
                 $response = Http::withBasicAuth(
                     config('services.nadpco_api.username'),
                     config('services.nadpco_api.password'),
-                )->post($this->url . 'v2/Token');
+                )->post(self::url . 'v2/Token');
                 return json_decode($response->body(), true)['token'];
             } catch (ConnectionException $e) {
                 dd($e->getMessage());
             }
         });
-        dd(self::getToken());
     }
 
-    public function companies()
+    public static function companies()
     {
-        $response = Http::get($this->url . '/v3/BaseInfo/Companies');
+        $response = Http::get(self::url . '/v3/BaseInfo/Companies');
         dd(json_decode($response->body(), true));
     }
 
-    public function indices()
+    public static function indices()
     {
         $response = Http::get('https://data.nadpco.com/v1/baseInfo/Indices');
         dd(json_decode($response->body(), true));
     }
 
-    public function trades($id)
+    public static function trades($id)
     {
         try {
             $response = Http::withToken(self::getToken())
                 ->withQueryParameters(['companyId' => $id])
-                ->get($this->url . '/v3/TS/RealTimeTradesToday');
+                ->get(self::url . '/v3/TS/RealTimeTradesToday');
             dd(json_decode($response->body(), true));
         } catch (ConnectionException $e) {
             dd($e->getMessage());
         }
     }
 
-    public function legalTrades($id)
+    public static function legalTrades($id)
     {
         try {
             $response = Http::withToken(self::getToken())
                 ->withQueryParameters(['companyId' => $id,])
-                ->get($this->url . '/v3/TS/RealTimeLegalTradesToday');
+                ->get(self::url . '/v3/TS/RealTimeLegalTradesToday');
             dd(json_decode($response->body(), true));
         } catch (ConnectionException $e) {
             dd($e->getMessage());
@@ -72,19 +75,19 @@ class ApiClient
         try {
             $response = Http::withToken(self::getToken())
                 ->withQueryParameters(['indexId' => $id])
-                ->get($this->url . '/v3/TS/RealTimeIndexValuesToday');
+                ->get(self::url . '/v3/TS/RealTimeIndexValuesToday');
             dd(json_decode($response->body(), true));
         } catch (ConnectionException $e) {
             dd($e->getMessage());
         }
     }
 
-    public function bidAsk($id)
+    public static function bidAsk($id)
     {
         try {
             $response = Http::withToken(self::getToken())
                 ->withQueryParameters(['companyId' => $id])
-                ->get($this->url . '/v3/TS/RealTimeBidAskToday');
+                ->get(self::url . '/v3/TS/RealTimeBidAskToday');
             dd(json_decode($response->body(), true));
         } catch (ConnectionException $e) {
             dd($e->getMessage());
