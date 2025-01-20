9a13d6b70c1a534bfd379257c1b8ec030b85f667
Arshia-r0 <arshia009k@proton.me>
2025-01-20 16:29:52 +0330

    tinymce dark mode

--- Modules/Admin/resources/views/components/tinymce.blade.php
+++ Modules/Admin/resources/views/components/tinymce.blade.php
@@ -2,6 +2,7 @@
 {{--        referrerpolicy="origin"></script>--}}
 <script src="/assets/admin/js/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
 <script>
+    const isDarkMode = getComputedStyle(document.documentElement).getPropertyValue('color-scheme').trim() === 'dark'
     tinymce.init({
         selector: '#adminEditor', // Replace this CSS selector to match the placeholder element for TinyMCE
         plugins: 'preview importcss searchreplace autolink autosave save directionality  visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons lists',
@@ -11,6 +12,8 @@
         license_key: 'gpl',
         file_picker_types: "file image media",
         promotion: false,
+        skin: isDarkMode ? "oxide-dark" : null,
+        content_css: isDarkMode ? "dark" : null,
         file_picker_callback(callback, value, meta) {
             let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth
             let y = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight
