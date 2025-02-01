7d0a22f9606710e3dd07027e5a84547803bda478
Arshia-r0 <arshia009k@proton.me>
2025-02-01 11:25:22 +0330

    fix j

---
+++ Modules/Auth/app/Models/Otp.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Auth\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// use Modules\Auth\Database\Factories\OtpFactory;
+
+class otp extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [];
+
+    // protected static function newFactory(): OtpFactory
+    // {
+    //     // return OtpFactory::new();
+    // }
+}
