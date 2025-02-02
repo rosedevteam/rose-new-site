ee4318b3a19c0efefe4e981a6ac1efb4ed487d5e
FarshadRajabzade <farshadrajabzade75@gmail.com>
2025-02-02 14:07:27 +0330

    seed real users

--- Modules/User/database/migrations/2024_11_16_074820_create_users_table.php
+++ Modules/User/database/migrations/2024_11_16_074820_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
             $table->id();
             $table->foreignId('creator_id')->nullable()->default(null)->constrained('users');
             $table->string('first_name')->nullable();
-            $table->string('last_name');
+            $table->string('last_name')->nullable();
             $table->string('phone')->unique();
             $table->string('email')->unique()->nullable();
-            $table->string('password')->nullable();
+            $table->string('password')->nullable()->default(null);
             $table->string('birthday')->nullable()->default(null);
             $table->string('avatar')->nullable();
             $table->rememberToken();
