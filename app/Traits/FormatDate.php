568126ade83741a73325da9295897be671ba7707
Arshia-r0 <arshia009k@proton.me>
2025-02-05 14:53:27 +0330

    fixformat datetime

--- app/Traits/FormatDate.php
+++ app/Traits/FormatDate.php
@@ -8,12 +8,21 @@ trait FormatDate
 {
     use ConvertNums;
 
-    protected static function formatDate(string $d)
+    protected static function formatDateTime(string $d)
     {
-        $expires_at = self::convertNums($d);
-        $t = explode(' ', $expires_at);
+        $d = self::convertNums($d);
+        $t = explode(' ', $d);
         $date = explode('/', $t[0]);
         $verta = Verta::jalaliToGregorian($date[0], $date[1], $date[2]);
         return $verta[0] . '/' . $verta[1] . '/' . $verta[2] . ' ' . $t[1];
     }
+
+    protected static function formatDate(string $d)
+    {
+        $d = self::convertNums($d);
+        $date = explode('/', $d);
+        $verta = Verta::jalaliToGregorian($date[0], $date[1], $date[2]);
+        return $verta[0] . '/' . $verta[1] . '/';
+    }
+
 }
