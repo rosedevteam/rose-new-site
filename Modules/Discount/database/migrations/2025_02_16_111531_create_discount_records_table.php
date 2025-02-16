795bcf06193fc1ab5089064b828a2b522a28fefe
Arshia-r0 <arshia009k@proton.me>
2025-02-16 11:55:13 +0330

    fix migration order

--- Modules/Discount/database/migrations/2025_02_16_111531_create_discount_records_table.php
+++ Modules/Discount/database/migrations/2025_02_16_111531_create_discount_records_table.php
@@ -24,6 +24,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('discount_uses');
+        Schema::dropIfExists('discount_records');
     }
 };
